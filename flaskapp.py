6ca0e7c9edcc928a73fa85e8409aaf624577b6b5
viet-hung <pham.viet-hung@agilent.com>
2018-09-19 16:10:48 +0800

    initial push
    initial

--- flaskapp.py
+++ flaskapp.py
@@ -5,24 +5,27 @@ Created on Fri Apr 20 21:49:34 2018
 @author: vietpham
 
 """
-from flask import Flask, render_template,request,send_file,jsonify,redirect, url_for, flash, make_response
+from flask import Flask, render_template,request,send_file,jsonify,redirect, url_for, flash, make_response, g
 
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, BooleanField, SubmitField
-from wtforms.validators import ValidationError, DataRequired, Email, EqualTo
+from wtforms import StringField, PasswordField, BooleanField, SubmitField, \
+    TextAreaField, SelectField
+from wtforms.validators import ValidationError, DataRequired, Email, EqualTo, \
+    Length
 from passlib.hash import pbkdf2_sha256 as sha256
 import json, datetime
 from werkzeug.wrappers import Response
 from flask_jwt_extended import JWTManager
 from flask_jwt_extended import (create_access_token, create_refresh_token, jwt_required, jwt_refresh_token_required, get_jwt_identity, jwt_optional)
+from flask_bootstrap import Bootstrap
 
 debug = True
 app = Flask(__name__)
 app.secret_key = '3000hanover'
 app.config['JWT_SECRET_KEY'] = 'jwt-secret-string'
-app.config['JWT_ACCESS_TOKEN_EXPIRES'] = datetime.timedelta(seconds=30)
+app.config['JWT_ACCESS_TOKEN_EXPIRES'] = datetime.timedelta(seconds=60)
 jwt = JWTManager(app)
-
+bootstrap = Bootstrap(app)
 #app.config['JWT_BLACKLIST_ENABLED'] = True
 #app.config['JWT_BLACKLIST_TOKEN_CHECKS'] = ['access', 'refresh']
 
@@ -52,8 +55,12 @@ class User(object):
 class LoginForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired()])
     password = PasswordField('Password', validators=[DataRequired()])
-    remember_me = BooleanField('Remember Me')
     submit = SubmitField('Sign In')
+    
+class QueryForm(FlaskForm):
+    type = SelectField(u'Type', choices = [('FA','FA'),('UEFA','UEFA'),('WC','WC')], validators = [DataRequired()])
+    match = SelectField(u'Match', choices = [], validators = [DataRequired()])
+    submit = SubmitField('Search')    
     
 # An example store of users. In production, this would likely
 # be a sqlalchemy instance or something similar.
@@ -121,12 +128,12 @@ def login():
     if verify_hash(password, user.password):
         access_token = create_access_token(identity=user.id)
         ret = {'access_token': access_token}
-        #return jsonify(ret), 200 
-        response = make_response("Hello World")
-        response.headers.set('Authorization', 'Bearer {0}'.format(access_token))        
-        response.headers.set('Cookie', request.headers.get('Cookie'))
-        print(response.headers)
-        return response
+        return jsonify(ret), 200 
+#        response = make_response("Hello World")
+#        response.headers.set('Authorization', 'Bearer {0}'.format(access_token))        
+#        response.headers.set('Cookie', request.headers.get('Cookie'))
+#        print(response.headers)
+#        return response
     else:
         return jsonify({"msg": "Password is wrong"}), 400
 
@@ -134,18 +141,100 @@ def login():
 @app.route('/user_login', methods=['GET'])
 def user_login():
     return render_template('login.html')
+#
+#@app.before_request
+#def apply_b4_caching(response):
+#    print('before ' + response.headers)
 
-
+@app.after_request
+def after(response):
+    # todo with response
+    val = getattr(g, 'auth', None)
+    if val is not None:
+        response.headers['Authorization'] = 'Bearer ' + str(val)
+    return response
+    
 # index page
-@app.route('/')
-@app.route('/index')
-@jwt_required
+@app.route('/query', methods=['GET', 'POST'])
+@jwt_optional
+def query():
+    current_user = get_jwt_identity()
+    print(current_user)
+    
+    form = QueryForm()
+    fa = ['AC Milan - ManU','Arsenal-Bayern','Chelsea-Liverpool']
+    fa_ch = []
+    for idx,val in enumerate(fa):
+        fa_ch.append((idx,val))
+    uefa = ['Barca-MU','Arseanl-Chelase']
+    uefa_ch = []
+    for idx,val in enumerate(uefa):
+        uefa_ch.append((idx,val))
+    wc = [u'Liverpool-NYC',u'NYC-SJS']
+    wc_ch = []
+    for idx,val in enumerate(wc):
+        wc_ch.append(( str(idx),val))
+    form.match.choices = wc_ch
+    
+    if form.validate_on_submit():
+        flash('Your post is now live!')
+        return render_template('query.html', title='Query',figmsg="Hello World", form=form )
+    return render_template('query.html', title='Query', form=form)
+    
+# index page
+@app.route('/', methods=['GET', 'POST'])
+@jwt_optional
 def index():
     # If no JWT is sent in with the request, get_jwt_identity()
     # will return None
     current_user = get_jwt_identity()
     print(current_user)
-    return "Hello World"
+    
+    if current_user:
+        print("in query")
+        form = QueryForm()
+        fa = ['AC Milan - ManU','Arsenal-Bayern','Chelsea-Liverpool']
+        fa_ch = []
+        for idx,val in enumerate(fa):
+            fa_ch.append((idx,val))
+        uefa = ['Barca-MU','Arseanl-Chelase']
+        uefa_ch = []
+        for idx,val in enumerate(uefa):
+            uefa_ch.append((idx,val))
+        wc = [u'Liverpool-NYC',u'NYC-SJS']
+        wc_ch = []
+        for idx,val in enumerate(wc):
+            wc_ch.append(( str(idx),val))
+        form.match.choices = wc_ch         
+        if form.validate_on_submit():
+            print('query submitted')
+            flash('Your query is now live!')
+            return render_template('query.html', title='Query',figmsg="Hello World", form=form )
+    else:
+        g.auth = ''
+        form = LoginForm()
+        if form.validate_on_submit():
+            print('login')
+            flash('You login')
+            access_token = create_access_token(identity=form.username.data)
+            form = QueryForm()
+            fa = ['AC Milan - ManU','Arsenal-Bayern','Chelsea-Liverpool']
+            fa_ch = []
+            for idx,val in enumerate(fa):
+                fa_ch.append((idx,val))
+            uefa = ['Barca-MU','Arseanl-Chelase']
+            uefa_ch = []
+            for idx,val in enumerate(uefa):
+                uefa_ch.append((idx,val))
+            wc = [u'Liverpool-NYC',u'NYC-SJS']
+            wc_ch = []
+            for idx,val in enumerate(wc):
+                wc_ch.append(( str(idx),val))
+            form.match.choices = wc_ch            
+            g.auth = access_token
+            return render_template('query.html', title='Query', form=form)
+        print('index')
+        return render_template('login.html', title='Sign In', form=form)
 
 # With debug=True, Flask server will auto-reload 
 # when there are code changes
--- static/mscript.js
+++ static/mscript.js
@@ -1,10 +1,11 @@
 $(function(){
     $("#btnSubmit").click(function(){
         $.ajax({
-            url: "/login", 
+            url: "/", 
             type: 'POST',          
             data: JSON.stringify({"username": $('#username').val(), 
-                                   "password": $('#password').val()
+                                   "password": $('#password').val(),
+                                   "csrf_token": $('#csrf_token').val()
                                 }),
             contentType: "application/json",
             dataType: "json",
@@ -21,9 +22,12 @@ $(function(){
     }); 
 });
 
-/* attach a submit handler to the form */
-$("#formoid").submit(function(event) {
 
+var authID = 'Bearer ';
+/* attach a submit handler to the form */
+//$("#myform").submit(function(event) {
+$("#submit").on('click',function(e)    {
+  alert( "Handler for .submit() called." );
   /* stop form from submitting normally */
   event.preventDefault();
 
@@ -31,18 +35,64 @@ $("#formoid").submit(function(event) {
   var $form = $( this ),
       url = $form.attr( 'action' );
 
+  if ($("#login").length>0) {
   /* Send the data using post with element id name and name2*/
   var posting = $.ajax({
-            url: "/user_login", 
-            type : 'POST',
-            data: JSON.stringify({"username": $('#username').val(), 
-                                   "password": $('#password').val()
+    url: url, 
+    type: 'POST',          
+    data: JSON.stringify({"username": $('#username').val(), 
+                                   "password": $('#password').val(),
+                                   "csrf_token": $('#csrf_token').val()
                                 }),
-            contentType: "application/json",
-            dataType: "json",
-            complete: function(data){
-                alert('hi');
-                console.log('hi');
-            }            
-            });
+    contentType: "application/json",
+    dataType: "json",        
+    complete: function(data){
+        if (data.status != 200) {
+            alert(data.responseJSON['msg']);
+            authID = 'Bearer ';
+        }
+        else {
+            console.log('login');
+            authID = data.getResponseHeader('authorization');
+            var d = jQuery(data.responseText);
+            for (var i=0;i<d.length;i++){
+                if (d[i].className == "container")
+                {
+                    console.log('change innerHTML');
+                    document.getElementById('container').outerHTML = d[i].outerHTML;
+                }
+            }
+            
+        }
+    }           
+    });
+
+  }
+  else {
+  /* Send the data using post with element id name and name2*/
+  var posting = $.ajax({
+    url: url, 
+    type: 'POST',          
+    data: JSON.stringify({"type": $('#type').val(), 
+                                   "match": $('#match').val(),
+                                   "csrf_token": $('#csrf_token').val()
+                                }),
+    contentType: "application/json",
+    dataType: "json",
+    beforeSend: function(xhr) {
+        xhr.setRequestHeader('Authorization', 'Bearer ' + authID);
+    },            
+    complete: function(data){
+        if (data.status != 200) {
+            alert(data.responseJSON['msg']);
+            authID = 'Bearer ';
+        }
+        else {
+            console.log('query');
+            authID = data.getResponseHeader('authorization');
+            document.documentElement.innerHTML = data.responseText;
+        }
+    }           
+    });  
+  }    
 });
